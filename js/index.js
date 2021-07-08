1585c63297c7f41a4db9a16f9eb0c5df40bbcd27
Daniel <daniel.migales.puertas@gmail.com>
2021-07-08 20:06:41 +0200

    v.1

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var personas = ["ataulfo", "teodorico", "atanagildo", "leogivildo", "sisebuto", "recesvinto", "ervigio"];
+var fechas = ["410-415", "418-451", "555-567", "571-586", "612-621", "653-672", "680-687"];
+
+// Següent línia si fas el bonus track
+var reis_afegits = [];
+
+window.onload = inici;
+
+function inici() {
+   // Posar el codi inicial i l'event onclick
+}
